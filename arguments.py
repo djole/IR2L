ac0b3816e57cad8bc8d2128f20f347084504dac2
Djordje Grbic <djgr@itu.dk>
2020-03-12 15:37:54 +0100

    Increase the number of goals to reach. Changes to be committed: 	modified:   arguments.py

--- arguments.py
+++ arguments.py
@@ -96,7 +96,7 @@ def get_args():
     )
     parser.add_argument(
         "--num-goal-samples",
-        default=2,
+        default=4,
         type=positive_nonzero_int,
         help='Defined how many steps in the cycle through goals',
     )
