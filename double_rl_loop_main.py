f8adccf6ae94190e0c2a2944be57b9cff539abab
calavera <dorde.grbic@gmail.com>
2021-03-23 15:44:09 +0100

    Bring box closer to the agent start for the push task.
    Randomize agent start position and reduce spawn area for objects in
    push-centered task.
    
     Changes to be committed:
    	modified:   double_rl_loop_main.py

--- double_rl_loop_main.py
+++ double_rl_loop_main.py
@@ -119,7 +119,7 @@ config_box['hazards_locations'] = [
                                     ]
 config_box['robot_locations'] = [(-4, 0), (-4, -2), (-4, 2)]
 config_box['goal_placements'] = [(3, -2, 4, 2)]
-config_box['box_placements'] = [(1, -2, 2, -1), (1, 1, 2, 2)]
+config_box['box_placements'] = [(-3, -2, -2, 2)]
 config_box['buttons_num'] = 1
 config_box['buttons_locations'] = [(-3, -3)]
 register(id=ENV_NAME_BOX,
@@ -139,9 +139,9 @@ config_box_no_haz['hazards_locations'] = [
                                     (-1, 2.5), (-1, 1.25), (-1, 0), (-1, -1.25), (-1, -2.5),
                                     (0, 2.5), (0, 1.25), (0, 0), (0, -1.25), (0, -2.5),
                                     ]
-config_box_no_haz['robot_locations'] = [(0, 0)]
-config_box_no_haz['goal_placements'] = [(-2, -2, 4, 4)]
-config_box_no_haz['box_placements'] = [(-2, -2, 4, 4)]
+config_box_no_haz['robot_locations'] = [] # [(0, 0)]
+config_box_no_haz['goal_placements'] = [(-2, -2, 2, 2)]
+config_box_no_haz['box_placements'] = [(-2, -2, 2, 2)]
 config_box_no_haz['buttons_num'] = 1
 config_box_no_haz['buttons_locations'] = [(-3, -3)]
 
